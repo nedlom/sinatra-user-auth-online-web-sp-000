764f9e8411a27b881fa5a0503c2db4ec8e877456
imperative-scientist-4600 <nelom3@gmail.com>
2021-04-30 17:24:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/registrations/signup' do
-
     erb :'/registrations/signup'
   end
 
   post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    puts params
+    @user = User.new(
+      name: params["name"], 
+      email: params["email"], 
+      password: params["password"]
+      )
     @user.save
     session[:user_id] = @user.id
 
@@ -31,6 +35,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
+  
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
@@ -45,7 +50,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
     @user = User.find(session[:user_id])
     erb :'/users/home'
   end
